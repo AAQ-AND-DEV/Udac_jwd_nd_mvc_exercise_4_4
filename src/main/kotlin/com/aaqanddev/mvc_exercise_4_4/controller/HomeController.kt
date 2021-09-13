a816788f50ab308159b812e6efa20d261a6e8117
aaq-and-dev <aaqjavadevmentor@gmail.com>
2021-09-13 13:43:14 -0700

    ** adds a model object and an attribute to the home page, sets it to print current timestamp

--- src/main/kotlin/com/aaqanddev/mvc_exercise_4_4/controller/HomeController.kt
+++ src/main/kotlin/com/aaqanddev/mvc_exercise_4_4/controller/HomeController.kt
@@ -1,14 +1,17 @@
 package com.aaqanddev.mvc_exercise_4_4.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import java.time.Instant
 
 //Spring MVC only looks for @Controller annotations not @Component
 @Controller
 class HomeController {
 
     @RequestMapping("/home")
-    fun getHomePage(): String{
+    fun getHomePage(model: Model): String{
+        model.addAttribute("welcomeMessage", Instant.now().toString())
         return "home"
     }
 
