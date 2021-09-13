770abd79548496fcbf7a6dd86f00f6c60371c6d9
aaq-and-dev <aaqjavadevmentor@gmail.com>
2021-09-13 13:26:00 -0700

    Initial commit

---
+++ src/main/kotlin/com/aaqanddev/mvc_exercise_4_4/MvcExercise44Application.kt
@@ -0,0 +1,11 @@
+package com.aaqanddev.mvc_exercise_4_4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MvcExercise44Application
+
+fun main(args: Array<String>) {
+	runApplication<MvcExercise44Application>(*args)
+}
